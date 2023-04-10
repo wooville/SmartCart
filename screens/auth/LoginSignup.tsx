7e91d6648fb2e2f62c5c2fb4ea607f25d343e565
Aboodnm <123132369+Aboodnm@users.noreply.github.com>
2023-04-10 16:38:46 -0400

    init styles login signup

--- screens/auth/LoginSignup.tsx
+++ screens/auth/LoginSignup.tsx
@@ -16,6 +16,7 @@ import { ItemData } from '../../utils/ProductListContext';
 import { ProductListContext } from '../../utils/ProductListContext';
 import { RootStackParamList } from '../RootStackParams';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { styles } from '../../styles/styles';
 
 const API_URL = 'http://smartcartbeanstalk-env.eba-3jmpa3xe.us-east-2.elasticbeanstalk.com/auth';
 
@@ -167,48 +168,3 @@ export const LoginSignup = () => {
     );
 }
 
-const styles = StyleSheet.create({
-    button: {
-        width: '75%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
-        height: 45,
-        borderRadius: 50,
-        marginVertical: 10,
-    },
-    separator: {
-        marginVertical: 5,
-    },
-    separator2: {
-        marginVertical: 3,
-    },
-    title: {
-        marginVertical: 8,
-        fontSize: 28,
-        fontWeight: 'bold',
-    },
-    buttontext: {
-        marginVertical: 5,
-        fontSize: 18,
-        color: 'white'
-    },
-    nextbutton: {
-        width: '75%',
-        backgroundColor: '#004b75',
-        height: 35,
-        borderRadius: 20,
-        marginVertical: 2,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    text: {
-        marginVertical: 3,
-        fontSize: 18,
-        color: 'white'
-    },
-    logo: {
-        width: 100,
-        height: 150
-    }
-});
---
+++ styles/styles.tsx
@@ -0,0 +1,52 @@
+import { StyleSheet } from "react-native";
+
+
+
+
+export const styles = StyleSheet.create({
+
+    button: {
+        width: '75%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        height: 45,
+        borderRadius: 50,
+        marginVertical: 10,
+    },
+    separator: {
+        marginVertical: 5,
+    },
+    separator2: {
+        marginVertical: 3,
+    },
+    title: {
+        marginVertical: 8,
+        fontSize: 28,
+        fontWeight: 'bold',
+    },
+    buttontext: {
+        marginVertical: 5,
+        fontSize: 18,
+        color: 'white'
+    },
+    nextbutton: {
+        width: '75%',
+        backgroundColor: '#004b75',
+        height: 35,
+        borderRadius: 20,
+        marginVertical: 2,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        marginVertical: 3,
+        fontSize: 18,
+        color: 'white'
+    },
+    logo: {
+        width: 100,
+        height: 150
+    }
+
+})
